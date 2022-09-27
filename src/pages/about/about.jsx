9578d7e7be4b999fb64dbcac441ea260840549df
AzraYazid <azrayazidalkautsar@gmail.com>
2022-09-27 16:49:07 +0700

    update UI using GSAP

--- src/pages/about/about.jsx
+++ src/pages/about/about.jsx
@@ -1,27 +1,96 @@
-import React, { useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 import Futer from '../../components/footer/footer';
-import Navbaar from '../../components/navbar/navbar';
+import { gsap, Power3 } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import { Link } from 'react-router-dom';
 import './about.css';
 
 export default function About() {
-  const [fadeIntxt, setFadeInText] = useState(false);
+  const fadeinAboutRef = useRef(null);
+  const dashboardRef = useRef(null);
+  const dashboardTextRef = useRef(null);
+  const dashboardButton = useRef(null);
+  gsap.registerPlugin(ScrollTrigger);
 
-  const changeBackgroundNavbar = () => {
-    // console.log(window.scrollY);
-    if (window.scrollY >= 400) {
-      setFadeInText(true);
-    } else {
-      setFadeInText(false);
-    }
-  };
+  useEffect(() => {
+    const el = fadeinAboutRef.current;
+    gsap.fromTo(
+      el,
+      {
+        opacity: 0,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        scrollTrigger: {
+          trigger: el,
+          start: 'top center',
+          end: 'top 100px',
+          scrub: true,
+          toggleActions: 'play reverse play reverse',
+        },
+      }
+    );
+  }, []);
 
-  window.addEventListener('scroll', changeBackgroundNavbar);
+  useEffect(() => {
+    const el = dashboardRef.current;
+    gsap.fromTo(
+      el,
+      {
+        opacity: 0,
+        y: -200,
+        ease: Power3.easeInOut,
+        duration: 0.8,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 0.8,
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    const el = dashboardButton.current;
+    gsap.fromTo(
+      el,
+      {
+        opacity: 0,
+        y: 200,
+        ease: Power3.easeInOut,
+        duration: 0.8,
+      },
+      {
+        opacity: 1,
+        y: 0,
+        duration: 0.8,
+      }
+    );
+  }, []);
+
+  useEffect(() => {
+    const el = dashboardTextRef.current;
+    gsap.fromTo(
+      el,
+      {
+        opacity: 0,
+        x: -200,
+        ease: Power3.easeInOut,
+        duration: 0.8,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 0.8,
+      }
+    );
+  }, []);
 
   return (
     <>
       <div className="App">
-        <Navbaar />
+        {/* <Navbaar /> */}
         {/* <div className="dashboarBio"> */}
         <section class="about-us pt-5" id="about-us">
           <div class="container">
@@ -29,20 +98,23 @@ export default function About() {
             <div class="row align-items-center">
               <div class="dashboarBio">
                 <span>
-                  <p class="glitch">about </p>
-                  <p class="fs-5">
+                  <p class="glitch" ref={dashboardRef}>
+                    about{' '}
+                  </p>
+                  <p class="fs-5" ref={dashboardTextRef}>
                     bio
                     <span style={{ color: '#13fbe2' }}> & </span>
                     resume
                   </p>
                   <a href="#aboutnih">
-                    <i class="bx bx-chevrons-down  fs-1" />
+                    <i class="bx bx-chevrons-down  fs-1" ref={dashboardButton} />
                   </a>
                 </span>
               </div>
             </div>
             {/* //////////////// */}
-            <div className={fadeIntxt ? 'row align-items-center fadeInAbout visible' : 'row align-items-center fadeInAbout'}>
+            {/* <div className={fadeIntxt ? 'row align-items-center fadeInAbout visible' : 'row align-items-center fadeInAbout'}> */}
+            <div className="row align-items-center fadeInAbout" ref={fadeinAboutRef}>
               <div class="dashboardAbout ">
                 <span>
                   <h2 id="aboutnih">I am currently working as aFull Stack Developerat public appraiser company in Jakarta. I enjoy building and enhance the web apps using Laravel and Vue JS . I also have a good taste inUI/UX Design</h2>

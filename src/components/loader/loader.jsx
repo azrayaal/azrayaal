f5456a8a8e4e3862d6d87c197696020117ba4b2a
azra <azrayazidalkautsar@gmail.com>
2023-11-28 13:50:50 +0700

    update loader

--- src/components/loader/loader.jsx
+++ src/components/loader/loader.jsx
@@ -1,23 +1,34 @@
 // import { Orbit } from '@uiball/loaders';
 
 export default function Loader() {
-  return (
-    <div className="bgloader">
-      <div className="triangles">
-        {/* <div className="triangles"> */}
-        <div className="tri invert"></div>
-        <div className="tri invert"></div>
-        <div className="tri"></div>
-        <div className="tri invert"></div>
-        <div className="tri invert"></div>
-        <div className="tri"></div>
-        <div className="tri invert"></div>
-        <div className="tri"></div>
-        <div className="tri invert"></div>
-        {/* </div> */}
-        {/* <HashLoader color="#36d7b7" size={80} />
-          <NewtonsCradle size={60} color="#36d7b7" /> */}
-      </div>
-    </div>
+  return (<></>
+    // <div className="bgloader">
+    //   <div className="triangles">
+    //     {/* <div className="triangles"> */}
+    //     <div className="tri invert"></div>
+    //     <div className="tri invert"></div>
+    //     <div className="tri"></div>
+    //     <div className="tri invert"></div>
+    //     <div className="tri invert"></div>
+    //     <div className="tri"></div>
+    //     <div className="tri invert"></div>
+    //     <div className="tri"></div>
+    //     <div className="tri invert"></div>
+    //     {/* </div> */}
+    //     {/* <HashLoader color="#36d7b7" size={80} />
+    //       <NewtonsCradle size={60} color="#36d7b7" /> */}
+    //   </div>
+    // </div>
   );
 }
+
+// import { helix } from 'ldrs'
+
+// helix.register()
+
+// // Default values shown
+// <l-helix
+//   size="45"
+//   speed="2.5" 
+//   color="black" 
+// ></l-helix>

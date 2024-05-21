42477ed35234a32d349e1f8409be59b0f2407d1f
azrayaal <azrayazidalkautsar@gmail.com>
2024-05-21 23:52:48 +0700

    update modal project

--- src/components/modals/modalsItem.jsx
+++ src/components/modals/modalsItem.jsx
@@ -1,16 +1,37 @@
-import React, { useState } from 'react';
-import { Modal } from 'react-bootstrap';
-import './modal.css';
+import React, { useState } from "react";
+import { Modal } from "react-bootstrap";
+import "./modal.css";
 
 export default function ModalsItem(props) {
-  const { handleClose, show, name, src, logo, renderHTML, desc, href, name2, src2, logo2, desc2, href2, tombolshow } = props;
+  const {
+    handleClose,
+    show,
+    name,
+    src,
+    logo,
+    renderHTML,
+    desc,
+    href,
+    name2,
+    src2,
+    logo2,
+    desc2,
+    href2,
+    tombolshow,
+  } = props;
   const [show2, setShow2] = useState(false);
   const handleShow = () => setShow2(true);
   const handleClose2 = () => setShow2(false);
 
   return (
     <>
-      <Modal size="xl" show={show} onHide={handleClose} keyboard="false" className="my-modal">
+      <Modal
+        size="xl"
+        show={show}
+        onHide={handleClose}
+        keyboard="false"
+        className="my-modal"
+      >
         <div class="container-fluids ">
           <figure>
             <img class="media" src={src}></img>
@@ -18,7 +39,7 @@ export default function ModalsItem(props) {
             <figcaption>
               <div class="bodymodal ">
                 <h3>{name}</h3>
-                <p style={{ textAlign: 'justify' }}>{renderHTML(`${desc}`)}</p>
+                <p style={{ textAlign: "justify" }}>{renderHTML(`${desc}`)}</p>
                 <div className="footermodal pb-2">
                   <div className="">{logo}</div>
                 </div>
@@ -30,17 +51,27 @@ export default function ModalsItem(props) {
               </button>
             </a>
             {tombolshow === true ? (
-              <button className="buttonmodal2" tombolshow={tombolshow} onClick={handleShow}>
+              <button
+                className="buttonmodal2"
+                tombolshow={tombolshow}
+                onClick={handleShow}
+              >
                 <span>See Admin</span>
               </button>
             ) : (
-              ''
+              ""
             )}
           </figure>
         </div>
       </Modal>
       {/* MODAL2 */}
-      <Modal size="xl" show={show2} onHide={handleClose} keyboard="false" className="my-modal">
+      <Modal
+        size="xl"
+        show={show2}
+        onHide={handleClose}
+        keyboard="false"
+        className="my-modal"
+      >
         <div class="container-fluids">
           <figure>
             <img class="media" src={src2}></img>
